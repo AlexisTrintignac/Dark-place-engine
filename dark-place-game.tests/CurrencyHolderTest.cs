9352006f8c868fa287e0441e7802414f92945799
Alexis Trintignac <alexis.trintignac@gmail.com>
2019-12-12 09:32:42 +0100

    ajout tests étape 6

--- dark-place-game.tests/CurrencyHolderTest.cs
+++ dark-place-game.tests/CurrencyHolderTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace dark_place_game.tests
 {
-    
+
     /// Cette classe contient tout un ensemble de tests unitaires pour la classe CurrencyHolder
     public class CurrencyHolderTest
     {
@@ -21,7 +21,7 @@ namespace dark_place_game.tests
         [Fact]
         public void CurrencyHolderCreatedWithInitialCurrentAmountOf10ShouldContain10Currency()
         {
-            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,EXEMPLE_CAPACITE_VALIDE , 10);
+            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, EXEMPLE_CAPACITE_VALIDE, 10);
             var result = ch.CurrentAmount == 10;
             Assert.True(result);
         }
@@ -37,7 +37,7 @@ namespace dark_place_game.tests
         [Fact]
         public void CurrencyHolderCreatedWithInitialCurrentAmountOf0ShouldContain0Currency()
         {
-            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,EXEMPLE_CAPACITE_VALIDE, 0);
+            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, EXEMPLE_CAPACITE_VALIDE, 0);
             var result = ch.CurrentAmount == 0;
             Assert.True(result);
         }
@@ -47,7 +47,7 @@ namespace dark_place_game.tests
         {
             // Petite subtilité : pour tester les levées d'exeption en c# on est obligé d'utiliser un concept un peu exotique : les expression lambda.
             // sans entrer dans le détail pour déclarer une lambda respectez la syntaxe ci dessous, pour rédiger des tests unitaires elle devrais vous suffire.
-            Action mauvaisAppel = () => new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,EXEMPLE_CAPACITE_VALIDE , -10);
+            Action mauvaisAppel = () => new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, EXEMPLE_CAPACITE_VALIDE, -10);
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
@@ -56,7 +56,7 @@ namespace dark_place_game.tests
         {
             // Petite subtilité : pour tester les levées d'exeption en c# on est obligé d'utiliser un concept un peu exotique : les expression lambda.
             // sans entrer dans le détail pour déclarer une lambda respectez la syntaxe ci dessous, pour rédiger des tests unitaires elle devrais vous suffire.
-            Action mauvaisAppel = () => new CurrencyHolder(null,EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel = () => new CurrencyHolder(null, EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
@@ -65,23 +65,23 @@ namespace dark_place_game.tests
         {
             // Petite subtilité : pour tester les levées d'exeption en c# on est obligé d'utiliser un concept un peu exotique : les expression lambda.
             // sans entrer dans le détail pour déclarer une lambda respectez la syntaxe ci dessous, pour rédiger des tests unitaires elle devrais vous suffire.
-            Action mauvaisAppel = () => new CurrencyHolder("",EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel = () => new CurrencyHolder("", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
         [Fact]
-        public void BrouzoufIsAValidCurrencyName ()
+        public void BrouzoufIsAValidCurrencyName()
         {
             // A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Brouzouf
-            var ch = new CurrencyHolder("Brouzouf",EXEMPLE_CAPACITE_VALIDE, 0);
+            var ch = new CurrencyHolder("Brouzouf", EXEMPLE_CAPACITE_VALIDE, 0);
             Assert.True(ch.CurrencyName == "Brouzouf");
         }
 
         [Fact]
-        public void DollardIsAValidCurrencyName ()
+        public void DollardIsAValidCurrencyName()
         {
             // A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Dollard
-            var ch = new CurrencyHolder("Dollard",EXEMPLE_CAPACITE_VALIDE, 0);
+            var ch = new CurrencyHolder("Dollard", EXEMPLE_CAPACITE_VALIDE, 0);
             Assert.True(ch.CurrencyName == "Dollard");
         }
 
@@ -89,7 +89,7 @@ namespace dark_place_game.tests
         public void TestPut10CurrencyInNonFullCurrencyHolder()
         {
             // A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac a moité plein, il contient maintenant la bonne quantité de currency
-            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,EXEMPLE_CAPACITE_VALIDE , 500);
+            var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, EXEMPLE_CAPACITE_VALIDE, 500);
             ch.Store(10);
             Assert.True(ch.CurrentAmount == 510);
         }
@@ -98,10 +98,11 @@ namespace dark_place_game.tests
         public void TestPut10CurrencyInNearlyFullCurrencyHolder()
         {
             // A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac quasiement plein, une exeption NotEnoughtSpaceInCurrencyHolderExeption est levée.
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 500, 490);
                 ch.Store(20);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
@@ -109,7 +110,7 @@ namespace dark_place_game.tests
         public void CreatingCurrencyHolderWithNameShorterThan4CharacterThrowExeption()
         {
             // A vous d'écrire un test qui doit échouer s'il es possible de créer un CurrencyHolder dont Le Nom De monnaie est inférieur a 4 lettres
-            Action mauvaisAppel = () => new CurrencyHolder("EU",EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel = () => new CurrencyHolder("EU", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
@@ -118,10 +119,11 @@ namespace dark_place_game.tests
         {
             // A vous d'écrire un test qui vérifie que retirer (methode withdraw) une quantité negative de currency leve une exeption CantWithDrawNegativeCurrencyAmountExeption.
             // Astuce : dans ce cas prévu avant même de pouvoir compiler le test, vous allez être obligé de créer la classe CantWithDrawMoreThanCurrentAmountExeption (vous pouvez la mettre dans le meme fichier que CurrencyHolder)
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 500, 490);
                 ch.Withdraw(-20);
-                };
+            };
             Assert.Throws<CantWithDrawMoreThanCurrentAmountExeption>(mauvaisAppel);
         }
 
@@ -129,22 +131,23 @@ namespace dark_place_game.tests
         public void CreatingCurrencyHolderWithNameBetween4And10CharacterThrowExeption()
         {
             // moins de 4 caractères
-            Action mauvaisAppel1 = () => new CurrencyHolder("EU",EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel1 = () => new CurrencyHolder("EU", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel1);
             // plus de 10 caractères
-            Action mauvaisAppel2 = () => new CurrencyHolder("EUUZDBFBUIFFFI",EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel2 = () => new CurrencyHolder("EUUZDBFBUIFFFI", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel2);
-            Action mauvaisAppel3 = () => new CurrencyHolder("EUUZDBFBUIFF",EXEMPLE_CAPACITE_VALIDE , EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Action mauvaisAppel3 = () => new CurrencyHolder("EUUZDBFBUIFF", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel3);
         }
 
         [Fact]
         public void StoreMoreThanCurrentAmountInCurrencyHolderThrowExeption()
         {
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 700, 490);
                 ch.Store(-20);
-                };
+            };
             Assert.Throws<CantStoreMoreThanCurrentAmountExeption>(mauvaisAppel);
         }
 
@@ -152,52 +155,68 @@ namespace dark_place_game.tests
         public void TestStore20CurrencyInNearlyFullCurrencyHolder()
         {
             // A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac quasiement plein, une exeption NotEnoughtSpaceInCurrencyHolderExeption est levée.
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 500, 490);
                 ch.Store(20);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
         [Fact]
         public void CantStoreAndWithdraw0CurrencyThrowExeption()
         {
-            Action mauvaisAppel1 = () => {
+            Action mauvaisAppel1 = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 700, 490);
                 ch.Store(0);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel1);
-            Action mauvaisAppel2 = () => {
+            Action mauvaisAppel2 = () =>
+            {
                 var ch = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, 700, 490);
                 ch.Withdraw(0);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel2);
         }
 
         [Fact]
         public void CantCreateCurrencyHolderWithAThrowExeption()
         {
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder("Absdfggr", 700, 490);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
         [Fact]
         public void CantCreateCurrencyHolderWithaThrowExeption()
         {
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder("abcsd", 700, 490);
-                };
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
         [Fact]
         public void CantCreateCurrencyHolderWith0CapacityThrowExeption()
         {
-            Action mauvaisAppel = () => {
+            Action mauvaisAppel = () =>
+            {
                 var ch = new CurrencyHolder("Test", 0, 0);
-                };
+            };
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+
+        [Fact]
+        public void CantCreateCurrencyHolderWith0NegativeCapacityThrowExeption()
+        {
+            Action mauvaisAppel = () =>
+            {
+                var ch = new CurrencyHolder("Test", -5, 0);
+            };
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
 
@@ -220,6 +239,8 @@ namespace dark_place_game.tests
         {
             var ch = new CurrencyHolder("Test", 500, 500);
             Assert.True(ch.IsFull());
+            var sac = new CurrencyHolder("Test", 800, 800);
+            Assert.True(sac.IsFull());
         }
 
         [Fact]
@@ -227,6 +248,8 @@ namespace dark_place_game.tests
         {
             var ch = new CurrencyHolder("Test", 500, 499);
             Assert.False(ch.IsFull());
+            var sac = new CurrencyHolder("Test", 800, 750);
+            Assert.False(sac.IsFull());
         }
     }
 }
--- dark-place-game/CurrencyHolder.cs
+++ dark-place-game/CurrencyHolder.cs
@@ -5,20 +5,22 @@ namespace dark_place_game
 
     [System.Serializable]
     /// Une Exeption Custom
-    public class NotEnoughtSpaceInCurrencyHolderExeption : System.Exception {}
+    public class NotEnoughtSpaceInCurrencyHolderExeption : System.Exception { }
 
-    public class CantWithDrawMoreThanCurrentAmountExeption : Exception{}
+    public class CantWithDrawMoreThanCurrentAmountExeption : Exception { }
 
-    public class CantStoreMoreThanCurrentAmountExeption : Exception{}
+    public class CantStoreMoreThanCurrentAmountExeption : Exception { }
 
     public class CurrencyHolder
     {
         public static readonly string CURRENCY_DEFAULT_NAME = "Unnamed";
 
         /// Le nom de la monnaie
-        public string CurrencyName {
-            get {return currencyName;}
-            private set {
+        public string CurrencyName
+        {
+            get { return currencyName; }
+            private set
+            {
                 currencyName = value;
             }
         }
@@ -26,9 +28,11 @@ namespace dark_place_game
         private string currencyName = CURRENCY_DEFAULT_NAME;
 
         /// Le montant actuel
-        public int CurrentAmount {
-            get {return currentAmount;}
-            private set {
+        public int CurrentAmount
+        {
+            get { return currentAmount; }
+            private set
+            {
                 currentAmount = value;
             }
         }
@@ -36,20 +40,25 @@ namespace dark_place_game
         private int currentAmount = 0;
 
         /// La contenance maximum du conteneur
-        public int Capacity {
-            get {return capacity;}
-            private set {
+        public int Capacity
+        {
+            get { return capacity; }
+            private set
+            {
                 capacity = value;
             }
         }
         // Le champs interne de la property
         private int capacity = 0;
 
-        public CurrencyHolder(string name,int capacity, int amount){
-            if(amount < 0 || name == null || name == "" || name.Length < 4 || name.Length > 10 || capacity < 1){
+        public CurrencyHolder(string name, int capacity, int amount)
+        {
+            if (amount < 0 || name == null || name == "" || name.Length < 4 || name.Length > 10 || capacity < 1)
+            {
                 throw new System.ArgumentException("Argument invalide");
             }
-            if(name[0]=='a' || name[0]=='A'){
+            if (name[0] == 'a' || name[0] == 'A')
+            {
                 throw new System.ArgumentException("Argument invalide");
             }
             Capacity = capacity;
@@ -57,40 +66,52 @@ namespace dark_place_game
             CurrentAmount = amount;
         }
 
-        public bool IsFull() {
-            if(this.CurrentAmount == this.Capacity){
+        public bool IsFull()
+        {
+            if (this.CurrentAmount == this.Capacity)
+            {
                 return true;
             }
             return false;
-            
+
         }
 
-        public bool IsEmpty() {
-            if (this.CurrentAmount == 0){
+        public bool IsEmpty()
+        {
+            if (this.CurrentAmount == 0)
+            {
                 return true;
             }
             return false;
         }
 
-        public void Store(int amount) {
+        public void Store(int amount)
+        {
             var amountCurrent = this.CurrentAmount + amount;
-            if(amount < 0) {
+            if (amount < 0)
+            {
                 throw new CantStoreMoreThanCurrentAmountExeption();
             }
-            if(amountCurrent > this.Capacity || amount == 0){
+            if (amountCurrent > this.Capacity || amount == 0)
+            {
                 throw new System.ArgumentException("Argument invalide");
-            }else {
+            }
+            else
+            {
                 this.CurrentAmount += amount;
             }
-            
+
         }
 
-        public void Withdraw(int amount) {
+        public void Withdraw(int amount)
+        {
             var amountCurrent = this.CurrentAmount - amount;
-            if(amount < 0){
+            if (amount < 0)
+            {
                 throw new CantWithDrawMoreThanCurrentAmountExeption();
             }
-            if(amount == 0) {
+            if (amount == 0)
+            {
                 throw new System.ArgumentException("Argument invalide");
             }
             this.CurrentAmount -= amount;
